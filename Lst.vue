917741fc0519b261a76c88ebb04582d0be1eae9c
lpf-1989 <lipeng.fei1989@163.com>
2019-09-21 13:04:46 +0800

    列表页

--- Lst.vue
+++ Lst.vue
@@ -123,146 +123,151 @@
     </div>
   </div>
 </template>
-<style lang="less">
+<style scoped>
   .header{
     padding: 0.5rem;
     display: flex;
     justify-content: space-between;  
-    .header_title{
+    
+  }
+  .header .header_title{
       font-weight: 700;
       color: #333333;
-    }
   }
   .address_date_select{
     padding: 1.5rem; 
     display: flex;
     justify-content: space-between;
-    .address_left{
-      display: flex;
-      align-items: center;
-      .distance_select{
-        display: flex;
-        align-items: center;      
-        .city{
-          margin-right: 5rem;
-          font-size: 0.8rem;
-          font-weight: 700;
-        }
-        .distance{
-          align-items: center;
-          img{
-            // margin-top: 0.3rem;
-          }
-        }
-      }
-    }
-    //右侧选择日期
-    .date_right{
-      .select_date{
-        font-size: 0.8rem;
-      }
-    }
+    
+    
   }
-  // 选择位置、筛选和排序
-  .select{
-    .select_ul{
-      display: flex;
-      justify-content: center;
-      li{
-        margin-right: 1rem;
-        display: flex;
-      }
-      li:last-child{
-        margin-right: 0;
-      }
-      span{
-        border-bottom: 1px solid #e0e0e0;
-      }
-    }
+  .address_date_select .address_left{
+    display: flex;
+    align-items: center;
+    
   }
-  // 酒店列表
+  .address_date_select .distance_select{
+    display: flex;
+    align-items: center;       
+  }
+  .address_date_select .distance_select .city{
+    margin-right: 5rem;
+    font-size: 0.8rem;
+    font-weight: 700;
+  }
+  .address_date_select .distance_select .distance{
+    align-items: center;
+  }
+  .address_date_select .distance_select .distance img{
+    margin-top: 0.3rem;
+  } 
+
+  /* 右侧选择日期 */
+  .address_date_select .date_right .select_date{
+    font-size: 0.8rem;
+  }
+
+  /* 选择位置、筛选和排序 */
+  .select .select_ul{
+    display: flex;
+    justify-content: center;  
+  }
+  .select .select_ul li{
+    margin-right: 1rem;
+    display: flex;
+  }
+  .select .select_ul li:last-child{
+    margin-right: 0;
+  }
+  .select .select_ul span{
+    border-bottom: 1px solid #e0e0e0;
+  }
+
+  /* 酒店列表 */
   .hotel_list{
     margin-top: 1rem;
-    .hotel{
-      position: relative;
-      .hotel_img{
-        width: 100%;
-        height: 15rem;
-        img{
-          width: 100%;
-          height: 100%;
-        }
-      }
-      .xin_collect{
-        position: absolute;
-        top: 0.6rem;
-        right: 0.7rem;
-      }
-      // 酒店详细信息
-      .hotel_info{
-        padding: 1rem;
-        position: relative;
-        .info_detail{
-          width: 75%;
-          .name{
-            font-size: 1.4rem;
-            font-weight: 500;
-            display: inline-block;
-            text-align: justify;
-          }
-          .desc{
-            color: #757575;
-            font-size: 0.8rem;
-            margin-top: 0.5rem;
-            display: inline-block;
-            text-align: left;
-          }
-          .tags{
-            margin-top: 0.5rem;
-            width: 22rem;
-            text-align: left;
-            span{
-              color: #720124;
-              font-size: 0.8rem;
-              img{
-                width: 0.75rem;
-                height: 0.75rem;
-                margin-left: 0.3rem;
-                margin-right: 0.3rem;
-              }
-              img:first-child{
-                margin-left: 0;
-              }
-            }
-          }
-        }
-        .head_photo{
-          position: absolute;
-          width: 3.5rem;
-          height: 3.5rem;
-          top: -2.1rem;
-          right: 1rem;
-          img{
-            width: 100%;
-            height: 100%;
-          }
-        }
-        .price{
-          display: inline-block;
-          width: 22rem;
-          text-align: left;
-          margin-top: 0.3rem;
-          span:first-child{
-            color: #ff4081;
-            font-size: 1.5rem;
-            margin-right: 0.2rem;
-          }
-          span:last-child{
-            font-size: 0.8rem;
-            color: #757575;
-          }
-        }
-      }
+  }
+  .hotel_list .hotel{
+      position: relative;  
     }
+  .hotel_list .hotel .hotel_img{
+    width: 100%;
+    height: 15rem;
+  }
+  .hotel_list .hotel .hotel_img img{
+    width: 100%;
+    height: 100%;
+  }
+  .hotel_list .hotel .xin_collect{
+    position: absolute;
+    top: 0.6rem;
+    right: 0.7rem;
+  }
+  /* 酒店详细信息 */
+  .hotel_list .hotel .hotel_info{
+    padding: 1rem;
+    position: relative;  
+  }
+  .hotel_list .hotel .hotel_info .info_detail{
+    width: 75%; 
+  }
+  .hotel_list .hotel .hotel_info .info_detail .name{
+      font-size: 1.4rem;
+      font-weight: 500;
+      display: inline-block;
+      text-align: justify;
+    }
+  .hotel_list .hotel .hotel_info .info_detail .desc{
+      color: #757575;
+      font-size: 0.8rem;
+      margin-top: 0.5rem;
+      display: inline-block;
+      text-align: left;
+    }
+  .hotel_list .hotel .hotel_info .info_detail .tags{
+    margin-top: 0.5rem;
+    width: 22rem;
+    text-align: left;  
+  }
+  .hotel_list .hotel .hotel_info .info_detail .tags span{
+    color: #720124;
+    font-size: 0.8rem;  
+  }
+  .hotel_list .hotel .hotel_info .info_detail .tags span img{
+    width: 0.75rem;
+    height: 0.75rem;
+    margin-left: 0.3rem;
+    margin-right: 0.3rem;
+  }
+  .hotel_list .hotel .hotel_info .info_detail .tags span img:first-child{
+    margin-left: 0;
+  }
+
+  .hotel_list .hotel .hotel_info .head_photo{
+    position: absolute;
+    width: 3.5rem;
+    height: 3.5rem;
+    top: -2.1rem;
+    right: 1rem; 
+  }
+  .hotel_list .hotel .hotel_info .head_photo img{
+    width: 100%;
+    height: 100%;
+  } 
+
+  .hotel_list .hotel .hotel_info .price{
+    display: inline-block;
+    width: 22rem;
+    text-align: left;
+    margin-top: 0.3rem; 
+  }
+  .hotel_list .hotel .hotel_info .price span:first-child{
+    color: #ff4081;
+    font-size: 1.5rem;
+    margin-right: 0.2rem;
+  }
+  .hotel_list .hotel .hotel_info .price span:last-child{
+    font-size: 0.8rem;
+    color: #757575;
   }
 </style>
